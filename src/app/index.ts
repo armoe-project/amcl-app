d3bf4e6da7b0c5500babcb073f58a4989a757f12
真心 <ceo@iqianye.cn>
2023-02-22 17:51:40 +0800

    [feat] initialize directory

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,11 +2,19 @@ import { useConfigStore } from '../store'
 import { logger, setBackground } from '../utils'
 import { createDir, exists } from '@tauri-apps/api/fs'
 import { appGlobal, setupAppGlobal } from './global'
-import { resolve } from '@tauri-apps/api/path'
+
+async function initializeDirectory() {
+  const dataDir = appGlobal.env.dataDir
+  if (!(await exists(dataDir))) await createDir(dataDir)
+
+  const backgroundDir = appGlobal.env.backgroundDir
+  if (!(await exists(backgroundDir))) await createDir(backgroundDir)
+
+  const tempDir = appGlobal.env.tempDir
+  if (!(await exists(tempDir))) await createDir(tempDir)
+}
 
 async function setupBackground() {
-  const backgroundDir = await resolve(appGlobal.env.dataDir, 'background')
-  if (!(await exists(backgroundDir))) await createDir(backgroundDir, { recursive: true })
   const configStore = useConfigStore()
   const background = configStore.background
   setBackground(background.type, background.network)
@@ -14,6 +22,7 @@ async function setupBackground() {
 
 async function setupApp() {
   await setupAppGlobal()
+  await initializeDirectory()
   await setupBackground()
 
   logger.info('Armoe Minecraft Launcher')
@@ -21,7 +30,6 @@ async function setupApp() {
   logger.info('Github: https://github.com/armoe-project/amcl-app')
 
   logger.debug(appGlobal)
-
 }
 
 export { setupApp, appGlobal }
