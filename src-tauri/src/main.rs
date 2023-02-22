3551a19157d923b6a1b80580fdf867e2603bfbb6
真心 <ceo@iqianye.cn>
2023-02-22 16:33:58 +0800

    [feat] handling environment exceptions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,6 @@
     windows_subsystem = "windows"
 )]
 
-#[cfg(target_os = "windows")]
-use fltk::{
-    app::{self, App},
-    button,
-    enums::{Color, FrameType},
-    prelude::{GroupExt, WidgetBase, WidgetExt, WindowExt},
-    text, window,
-};
 use tauri::Manager;
 
 fn main() {
@@ -22,43 +14,18 @@ fn main() {
         Ok(())
     });
 
-    #[cfg(not(target_os = "windows"))]
     builder
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .unwrap_or(runtime_error())
+}
 
+fn runtime_error() {
     #[cfg(target_os = "windows")]
-    builder
-        .run(tauri::generate_context!())
-        .unwrap_or(create_window());
-}
-
-#[cfg(target_os = "windows")]
-fn create_window() {
-    let app = app::App::default();
-    let mut window =
-        window::Window::new(0, 0, 425, 125, "Armoe Minecraft Launcher").center_screen();
-    window.set_color(Color::White);
-    let mut txt = text::TextDisplay::default()
-        .with_label("您可能未安装必要运行环境，点击 OK 查看帮助文档.\nYou may not have the necessary runtime environment installed.\nclick OK to view the help file.")
-        .with_size(200, 10)
-        .center_of_parent();
-    txt.set_frame(FrameType::RFlatBox);
-
-    let mut btn = button::Button::new(txt.x() + 50, txt.y() + 20, 80, 30, None);
-    btn.set_label("OK");
-    btn.set_color(Color::White);
-    btn.set_frame(FrameType::BorderBox);
-    window.end();
-    window.show();
-    window.set_callback(move |_| open_docs(app));
-    btn.set_callback(move |_| open_docs(app));
-    app.run().unwrap();
-}
-
-#[cfg(target_os = "windows")]
-fn open_docs(app: App) {
-    opener::open("https://amcl.armoe.cn/install").unwrap();
-    app::sleep(0.1);
-    app.quit();
+    let url = "https://amcl.armoe.cn/env/windows.html";
+    #[cfg(target_os = "macos")]
+    let url = "https://amcl.armoe.cn/env/macos.html";
+    #[cfg(target_os = "linux")]
+    let url = "https://amcl.armoe.cn/env/linux.html";
+    opener::open(url).unwrap();
+    std::process::exit(0)
 }
