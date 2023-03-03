b32070efd9ae4db05cc05a7a04c342e999cfb69a
真心 <qgzhenxin@qq.com>
2023-03-04 00:11:59 +0800

    [chore] change window size

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,21 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
+use tauri::{Manager, PhysicalSize, Size};
 use window_shadows::set_shadow;
 
 fn main() {
     let builder = tauri::Builder::default().setup(|app| {
         let window = app.get_window("main").unwrap();
-        #[cfg(not(target_os = "macos"))]
-        window.set_decorations(false).unwrap();
+        if cfg!(not(target_os = "macos")) {
+            window.set_decorations(false).unwrap();
+            window
+                .set_size(Size::Physical(PhysicalSize {
+                    width: 820,
+                    height: 461,
+                }))
+                .unwrap();
+        }
         #[cfg(not(target_os = "linux"))]
         set_shadow(&window, true).unwrap();
         window.show().unwrap();
